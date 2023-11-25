793b4154c623d3d2f49defd33f1f5bf2fa4e4756
Gaspberry <GASPBERRY1337@gmail.com>
2023-11-24 23:57:31 -0500

    Shopping Cart house keeping

--- Shop/script.js
+++ Shop/script.js
@@ -23,4 +23,47 @@ for (const item of cartItems) {
 // Calculate the total price of the cart
 const totalPrice = cartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
 
-//
+// Add forms of payment
+const paymentMethods = [
+    'American Express',
+    'Amazon Pay',
+    'Samsung Pay',
+    'Google Pay',
+    'Apple Pay',
+    'PayPal',
+    'Zelle',
+    'Shop Pay',
+    'Bitcoin'
+];
+
+// Create a container for payment methods
+const paymentContainer = document.createElement('div');
+paymentContainer.classList.add('payment-container');
+
+// Add each payment method to the container
+for (const method of paymentMethods) {
+    const paymentMethod = document.createElement('div');
+    paymentMethod.textContent = method;
+    paymentContainer.appendChild(paymentMethod);
+}
+
+// Append the payment container to the body
+document.body.appendChild(paymentContainer);
+
+// Fetch Bitcoin price initially and then every 5 minutes (300,000 milliseconds)
+async function fetchBitcoinPrice() {
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+        const data = await response.json();
+        const bitcoinPrice = data.bitcoin.usd;
+
+        const bitcoinPriceElement = document.getElementById('bitcoin-price');
+        bitcoinPriceElement.textContent = `$${bitcoinPrice}`;
+    } catch (error) {
+        console.error('Error fetching Bitcoin price:', error.message);
+    }
+}
+
+// Fetch Bitcoin price initially and then every 5 minutes (300,000 milliseconds)
+fetchBitcoinPrice();
+setInterval(fetchBitcoinPrice, 300000); // Update every 5 minutes
