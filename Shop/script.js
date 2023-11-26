a37f0eceaf62b46c24befc1201feb9ed081fb5c5
Gaspberry <GASPBERRY1337@gmail.com>
2023-11-25 20:41:20 -0500

    made both pages more RESPONSIVE for smaller device

--- Shop/script.js
+++ Shop/script.js
@@ -1,69 +1,25 @@
-// Get the cart items table
-const cartTable = document.querySelector('.cart-table tbody');
-
-// Add a row to the table for each cart item
-for (const item of cartItems) {
-    const row = document.createElement('tr');
-
-    const productCell = document.createElement('td');
-    productCell.textContent = item.product.name;
-    row.appendChild(productCell);
-
-    const quantityCell = document.createElement('td');
-    quantityCell.textContent = item.quantity;
-    row.appendChild(quantityCell);
-
-    const priceCell = document.createElement('td');
-    priceCell.textContent = `$${item.product.price}`;
-    row.appendChild(priceCell);
-
-    cartTable.appendChild(row);
-}
-
-// Calculate the total price of the cart
-const totalPrice = cartItems.reduce((total, item) => total + item.product.price * item.quantity, 0);
-
-// Add forms of payment
-const paymentMethods = [
-    'American Express',
-    'Amazon Pay',
-    'Samsung Pay',
-    'Google Pay',
-    'Apple Pay',
-    'PayPal',
-    'Zelle',
-    'Shop Pay',
-    'Bitcoin'
-];
-
-// Create a container for payment methods
-const paymentContainer = document.createElement('div');
-paymentContainer.classList.add('payment-container');
-
-// Add each payment method to the container
-for (const method of paymentMethods) {
-    const paymentMethod = document.createElement('div');
-    paymentMethod.textContent = method;
-    paymentContainer.appendChild(paymentMethod);
-}
-
-// Append the payment container to the body
-document.body.appendChild(paymentContainer);
-
-// Fetch Bitcoin price initially and then every 5 minutes (300,000 milliseconds)
-async function fetchBitcoinPrice() {
-    try {
-        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
-        const data = await response.json();
-        const bitcoinPrice = data.bitcoin.usd;
-
-        const bitcoinPriceElement = document.getElementById('bitcoin-price');
-        bitcoinPriceElement.textContent = `$${bitcoinPrice}`;
-    } catch (error) {
-        console.error('Error fetching Bitcoin price:', error.message);
+// Add event listener to the menu toggle button
+$(document).ready(function () {
+    const menuToggle = $('.menu-toggle');
+    const nav = $('nav');
+  
+    menuToggle.on('click', function () {
+      nav.toggleClass('active');
+    });
+  
+    // Fetch Bitcoin price from CoinGecko API
+    function fetchBitcoinPrice() {
+      $.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+        .done(function (data) {
+          const bitcoinPrice = data.bitcoin.usd;
+          $('#bitcoin-price').text(`$${bitcoinPrice.toFixed(2)}`); // Display Bitcoin price with 2 decimal places
+        })
+        .fail(function (error) {
+          console.error('Error fetching Bitcoin price:', error.statusText);
+        });
     }
-}
-
-// Fetch Bitcoin price initially and then every 5 minutes (300,000 milliseconds)
-fetchBitcoinPrice();
-setInterval(fetchBitcoinPrice, 300000); // Update every 5 minutes
+  
+    // Fetch Bitcoin price initially and then every 1 minute (60,000 milliseconds)
+    fetchBitcoinPrice();
+    setInterval(fetchBitcoinPrice, 60000); // Update every 1 minute
+});
--- language.js
+++ language.js
@@ -1,24 +1,44 @@
 // Add event listener to the menu toggle button
 $(document).ready(function () {
   const menuToggle = $('.menu-toggle');
+  const nav = $('nav');
+
   menuToggle.on('click', function () {
-    const nav = $('nav');
     nav.toggleClass('active');
   });
 
   // Fetch Bitcoin price from CoinGecko API
-  // Fetch Bitcoin price initially and then every 1 minute (60,000 milliseconds)
   function fetchBitcoinPrice() {
-    $.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd', function (data) {
-      const bitcoinPrice = data.bitcoin.usd;
-      $('#bitcoin-price').text(`$${bitcoinPrice}`);
-    })
-    .fail(function (error) {
-      console.error('Error fetching Bitcoin price:', error.statusText);
-    });
+    $.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+      .done(function (data) {
+        const bitcoinPrice = data.bitcoin.usd;
+        $('#bitcoin-price').text(`$${bitcoinPrice.toFixed(2)}`); // Display Bitcoin price with 2 decimal places
+      })
+      .fail(function (error) {
+        console.error('Error fetching Bitcoin price:', error.statusText);
+      });
   }
 
   // Fetch Bitcoin price initially and then every 1 minute (60,000 milliseconds)
   fetchBitcoinPrice();
   setInterval(fetchBitcoinPrice, 60000); // Update every 1 minute
+
+  // Additional Bitcoin-related code from script.js_1
+  // Fetch Bitcoin price initially and then every 5 minutes (300,000 milliseconds)
+  async function fetchBitcoinPriceExtended() {
+    try {
+      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+      const data = await response.json();
+      const bitcoinPrice = data.bitcoin.usd;
+
+      const bitcoinPriceElement = document.getElementById('bitcoin-price');
+      bitcoinPriceElement.textContent = `$${bitcoinPrice}`;
+    } catch (error) {
+      console.error('Error fetching Bitcoin price:', error.message);
+    }
+  }
+
+  // Fetch Bitcoin price initially and then every 5 minutes (300,000 milliseconds)
+  fetchBitcoinPriceExtended();
+  setInterval(fetchBitcoinPriceExtended, 300000); // Update every 5 minutes
 });
